235e42c0202a9a4798ddad80b3522b5859688fc1
susan2929 <117727275+susan2929@users.noreply.github.com>
2022-12-26 09:19:19 +0545

    Add files via upload

---
+++ hire.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>hire form</title>
+</head>
+
+<body>
+  <div class="hiring">
+
+    <div id="hire">
+
+      <h1><span>want to</span> hire me</h1>
+      <p>please fill up the form</p>
+      <div class="part1">
+        <input type="text" placeholder="your name"><br><br>
+
+
+        <input type="number" placeholder="your phone number"><br><br>
+      </div>
+
+      <div class="part2">
+        <input type="email" placeholder="your email"><br><br>
+
+
+        <input type="text" placeholder="your website url"><br><br>
+      </div>
+
+
+      <div class="part3">
+        <input type="text" placeholder="your company name"><br><br>
+
+
+
+        <select id="budget">
+          <option>budget range</option>
+          <option>10$-50$</option>
+          <option>50$-100$</option>
+          <option>100$-250$</option>
+          <option>250$-500$</option>
+
+        </select><br><br>
+      </div>
+
+
+      <div class="btn7">
+        <a href="#">submit</a>
+        <a href="#">reset</a>
+      </div>
+
+
+    </div>
+
+
+
+    <div class="touch">
+      <p>i will be in touch as soon as possible!</p>
+    </div>
+
+
+
+  </div>
+</body>
+
+</html>
